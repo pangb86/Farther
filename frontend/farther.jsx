004bdc6483db89457ce781f695a8f06da1bfc4f5
Bo <pangb86@gmail.com>
2017-11-21 18:31:00 -0800

    Complete session API utils

--- frontend/farther.jsx
+++ frontend/farther.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as SessionAPIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // test functions
+  window.login = SessionAPIUtil.login;
+  window.signup = SessionAPIUtil.signup;
+  window.logout = SessionAPIUtil.logout;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Farther</h1>, root);
 });
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -5,3 +5,18 @@ export const login = user => (
     data: user
   })
 );
+
+export const signup = user => (
+  $.ajax({
+    method: "POST",
+    url: "api/users",
+    data: user
+  })
+);
+
+export const logout = () => (
+  $.ajax({
+    method: "DELETE",
+    url: "api/session"
+  })
+);
