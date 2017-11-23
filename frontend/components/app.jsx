6c4e2eb876ab5deb885535634c1cacaf88f96cb5
Bo <pangb86@gmail.com>
2017-11-23 09:46:10 -0800

    Add AuthRoute and ProtectedRoute functionality

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavbarContainer from './navbar/navbar_container';
 import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div className="outer-main-div">
     <NavbarContainer />
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
