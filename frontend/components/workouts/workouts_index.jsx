4ee3411dc4589d880aa07dedb3a4f0d56c61cc2a
Bo <pangb86@gmail.com>
2017-11-30 14:23:12 -0800

    Add CSS and workout totals for workout index page

--- frontend/components/workouts/workouts_index.jsx
+++ frontend/components/workouts/workouts_index.jsx
@@ -21,7 +21,41 @@ class WorkoutsIndex extends React.Component {
     if (this.props.workouts[0]) {
       const routesArr = this.props.routes;
       const workoutsArr = this.props.workouts;
-      
+      // declare variables for workouts stats
+      let totalDistance = 0;
+      let averageDistance = 0;
+      let totalElevation = 0;
+      let averageElevation = 0;
+      let totalSpeed = 0;
+      let averageSpeed = 0;
+      let totalTime = 0;
+      // calculates workout stat totals
+      for (var i = 0; i < workoutsArr.length; i++) {
+        const currentWorkout = workoutsArr[i];
+        const currentRoute = routesArr.find((route) => route.id === currentWorkout.route_id);
+        totalDistance += currentRoute.distance;
+        totalElevation += currentRoute.elevation;
+        totalSpeed += currentWorkout.speed;
+        totalTime += currentWorkout.duration;
+      }
+      // caclulate workout average stats
+      averageDistance = Math.round(totalDistance / workoutsArr.length * 10) / 10;
+      averageElevation = Math.round(totalElevation / workoutsArr.length);
+      averageSpeed = Math.round(totalSpeed / workoutsArr.length * 10) / 10;
+      totalDistance = Math.round(totalDistance * 10) / 10;
+      // calculate total workout time in hours and minutes
+      let hours = Math.floor(totalTime / 3600);
+      let minutes = Math.round((totalTime % 3600) / 60);
+      // converts the total time into a string
+      let timeString = "";
+      if (hours === 0) {
+        timeString = `${minutes} min`;
+      } else if (minutes === 0) {
+        timeString = `${hours} hr`;
+      } else {
+        timeString = `${hours} hr ${minutes} min`;
+      }
+
       return(
         <div className="workouts-index-box">
 
@@ -32,6 +66,39 @@ class WorkoutsIndex extends React.Component {
                 Create New Workout
               </button>
             </Link>
+          </div>
+          <div className="workouts-stats-box">
+            <div className="workouts-index-number">
+              {workoutsArr.length}
+              <div className="workouts-stat-workouts-tag">
+                Workouts
+              </div>
+            </div>
+            <div className="workouts-index-number">
+              {timeString}
+              <div className="workouts-stat-workouts-tag">
+                Total Time
+              </div>
+            </div>
+            <div className="workouts-index-number">
+              {`${totalDistance} mi`}
+              <br/>
+              <div className="workouts-stat-workouts-tag">
+                Total Distance
+              </div>
+            </div>
+            <div className="workouts-index-number">
+              {`${totalElevation} ft`}
+              <div className="workouts-stat-workouts-tag">
+                Total Elevation
+              </div>
+            </div>
+            <div className="workouts-index-number">
+              {`${averageSpeed} mph`}
+              <div className="workouts-stat-workouts-tag">
+                Average Speed
+              </div>
+            </div>
           </div>
 
           <div className="workouts-index-list-box">
