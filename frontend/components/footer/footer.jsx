6443532d76aa4325ad0f31d629dd8a875afe6f5f
Bo <pangb86@gmail.com>
2017-12-01 15:50:08 -0800

    Fix routes creation image

---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer-main">
+      <div className="footer-links-one">
+        <div className="footer-one-header">
+          Affiliations
+        <div>
+        <a href="https://www.appacademy.io/" target="_blank">
+          App Academy
+        </a>
+        <a href="https://www.appacademy.io/" target="_blank">
+          The Ohio State University
+        </a>
+      </div>
+
+      <div className="footer-links-two">
+        <div className="footer-one-header">
+          Social
+        <div>
+        <a href="https://github.com/pangb86" target="_blank">
+          Github
+        </a>
+
+      </div>
+    </div>
+  );
+};
+
+// ```import React from 'react';
+//
+// const Footer = () => {
+//  return (
+//    <div className="footer">
+//        <a href="http://github.com/henrychen11" target="_blank">
+//          <i className="fa fa-github" aria-hidden="true"></i>
+//        </a>
+//
+//        <a href="http://www.linkedin.com/in/henrychen11/" target="_blank">
+//          <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+//        </a>
+//    </div>
+//  );
+// };
+//
+// export default Footer;```
